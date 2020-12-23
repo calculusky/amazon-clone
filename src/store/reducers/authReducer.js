2382e80ac64b822f81486c95c55013e98c4910dd
calculusky <calculusky@gmail.com>
2020-12-23 16:17:30 +0100

    dec23

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,20 +1,22 @@
 import * as actionTypes from '../constants/constants';
 
-const initialState = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : { email: '', password: ''};
+const initialState = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : { loading: true };
 export const signinReducer = ( state = initialState, action) => {
     switch(action.type){
         case actionTypes.SIGNIN_START:
             return {
-                userInfo: initialState
+                loading: true  
             }
         
         case actionTypes.SIGNIN_SUCCESS: 
         return {
+            loading: false,
             userInfo: action.payload
         }
 
         case actionTypes.SIGNIN_FAILURE:
             return {
+                loading: false,
                 error: action.payload
             }
 
