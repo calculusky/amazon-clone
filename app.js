b766f5a5ba9c7440401e00a024c14d07beb5671f
calculusky <calculusky@gmail.com>
2020-12-28 08:08:45 +0100

    dec29

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
@@ -7,6 +8,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 //register middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
